1e61017e4c23bf80a847ceee62d3c064db749005
shiguanghai <1932275668@qq.com>
2021-01-02 17:27:27 +0800

    03-03更新..

--- README.md
+++
@@ -1,3 +0,0 @@
-## 拉勾大前端高薪训练营笔记
-
-同样欢迎来到我的博客主页：[shiguanghai.top](http://shiguanghai.top/)
--- fed-e-task-03-03/code/3-3-4-1-nuxtjs综合案例/realworld-nuxtjs/api/tag.js
+++
@@ -1,9 +0,0 @@
-import { request } from '@/plugins/request'
-
-// 获取文章标签列表
-export const getTags = () => {
-  return request({
-    method: 'GET',
-    url: '/api/tags'
-  })
-}
